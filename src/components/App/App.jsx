16d669c34944b62073379d714686c0b901c9e91f
Khavkin <hdm@ukr.net>
2023-03-10 19:17:01 +0200

    clear test state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,15 +5,15 @@ import { nanoid } from 'nanoid';
 import Filter from 'components/Filter';
 import ContactList from 'components/ContactList';
 
-const test = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-  filter: '',
-};
+// const test = {
+//   contacts: [
+//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   ],
+//   filter: '',
+// };
 
 class App extends Component {
   defaultState = {
@@ -21,7 +21,7 @@ class App extends Component {
     filter: '',
   };
 
-  state = test; //this.defaultState;
+  state = { ...this.defaultState };
 
   handleOnSubitContactForm = contact => {
     if (!this.findContact(contact.name)) {
